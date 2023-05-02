29f665e8e4513a0f3d3e217fd67095437e5d84ae
Santiago Carmuega <santiago@carmuega.me>
2023-05-01 23:08:02 -0300

    feat: setup crate

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub mod proto {
+    pub mod cardano {
+        pub use utxorpc_spec_cardano::utxorpc::cardano::v1;
+    }
+
+    pub mod build {
+        pub use utxorpc_spec_build::utxorpc::build::v1;
+    }
+
+    pub mod sync {
+        pub use utxorpc_spec_sync::utxorpc::sync::v1;
+    }
+
+    pub mod submit {
+        pub use utxorpc_spec_submit::utxorpc::submit::v1;
+    }
+
+    pub mod watch {
+        pub use utxorpc_spec_watch::utxorpc::watch::v1;
+    }
+}
