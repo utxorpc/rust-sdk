76f08be051e8c6c057d15d5e89d20802160b1deb
Clark Alesna <clark_alesna@hotmail.com>
2024-02-12 19:11:59 +0800

    chore: update spec to v0.3.0 and improve imports (#2)
    This commit restructures the proto module to nest cardano, sync, watch, submit, and build submodules under a new v1alpha namespace.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,28 @@
 pub mod proto {
-    pub mod cardano {
-        pub use utxorpc_spec_cardano::utxorpc::cardano::v1;
+    pub mod v1alpha {
+        // Expose the 'cardano' module
+        pub mod cardano {
+            pub use utxorpc_spec::utxorpc::v1alpha::cardano::*;
+        }
+
+        // Expose the 'sync' module
+        pub mod sync {
+            pub use utxorpc_spec::utxorpc::v1alpha::sync::*;
+        }
+
+        // Expose the 'watch' module
+        pub mod watch {
+            pub use utxorpc_spec::utxorpc::v1alpha::watch::*;
+        }
+
+        // Expose the 'submit' module
+        pub mod submit {
+            pub use utxorpc_spec::utxorpc::v1alpha::submit::*;
+        }
+
+        // Expose the 'build' module
+        pub mod build {
+            pub use utxorpc_spec::utxorpc::v1alpha::build::*;
+        }
     }
-
-    // pub mod build {
-    //     pub use utxorpc_spec_build::utxorpc::build::v1;
-    // }
-
-    pub mod sync {
-        pub use utxorpc_spec_sync::utxorpc::sync::v1;
-    }
-
-    // pub mod submit {
-    //     pub use utxorpc_spec_submit::utxorpc::submit::v1;
-    // }
-
-    // pub mod watch {
-    //     pub use utxorpc_spec_watch::utxorpc::watch::v1;
-    // }
 }
